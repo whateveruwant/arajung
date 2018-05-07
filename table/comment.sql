228f8838886d308d0c2825e25ee613591dbe61bf
whateveruwant <Yumin@ASUS-PYM.localdomain>
2018-05-07 14:13:25 +0900

    remove tables

--- table/comment.sql
+++
@@ -1,9 +0,0 @@
-create table tbl_comment(
-  num int(11) not null auto_increment,
-  parent_num int default 0,
-  nick_name char(30) null,
-  content text null,
-  regist_day char(30) null,
-  level char(30) null,
-  primary key(num)
-) default charset=utf8;
